7c670eaebf4701ee35b9721bd305c456c53570ea
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2022-09-07 00:23:51 -0300

    chore: users table created

---
+++ prisma/migrations/20220907032208_create_users_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
