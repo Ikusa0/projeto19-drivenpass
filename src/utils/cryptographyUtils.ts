dd5ca8f8e5964ae0489ab7e8bc401435ce3d78fd
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2022-09-07 03:21:09 -0300

    chore: added cryptr utils

--- src/utils/cryptographyUtils.ts
+++ src/utils/cryptographyUtils.ts
@@ -1,11 +1,26 @@
+import dotenv from "dotenv";
 import bcrypt from "bcrypt";
+import Cryptr from "cryptr";
+
+dotenv.config();
+const cryptr = new Cryptr(process.env.CRYPTR_SECRET);
 
 export function hashString(text: string): string {
   const hashedString: string = bcrypt.hashSync(text, 10);
   return hashedString;
 }
 
-export function compareHashedString(hashed: string, plain: string) {
+export function compareHashedString(hashed: string, plain: string): boolean {
   const match: boolean = bcrypt.compareSync(plain, hashed);
   return match;
+}
+
+export function encryptString(text: string): string {
+  const encryptedString = cryptr.encrypt(text);
+  return encryptedString;
+}
+
+export function decryptString(encrypted: string): string {
+  const decryptedString = cryptr.decrypt(encrypted);
+  return decryptedString;
 }
